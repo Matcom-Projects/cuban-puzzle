ce9b4dcf3840fa6acee935f3fa206a7ed24d8728
Kpiro <106298160+Kpiro@users.noreply.github.com>
2022-12-22 17:02:39 -0500

    5:02pm 22/12 (terminada ActionPhase)

--- Pruebas/GameEngine.cs
+++ Pruebas/GameEngine.cs
@@ -1,122 +1,107 @@
 using System.Collections.Generic;
 
-namespace AppConsole
+namespace AppConsole//cambios
 {
-    public class GameUtils
-    {
-        public static void Move ( List<Card> a , List<Card> b , int index )
-        {
-            b.Add(a[index]);
-            a.RemoveAt(index);
-        }
-        public static List<IPlayer> Mezclar(List<IPlayer> a)
-        {
-            List<IPlayer> result = new List<IPlayer>();    
-            int index;
-
-            while(a.Count != 0)
-            {
-                index = GetRandom(0,a.Count);//aqui creo que es a.Count-1 pq si el count==4, no se puede indexar en ese valor pq se va de los limites del array 
-                result.Add(a[index]);
-                a.RemoveAt(index);
-            }
-
-            return result;
-        }
-
-        public static int GetRandom(int min,int max)
-        {
-            Random e = new Random();
-            return e.Next(min,max);
-        }
-        public static string CreateId ()//para cuando se haga la ast
-        {
-            string letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-            string result = "";
-            Random e = new Random();
-
-            for( int i = 0 ; i < 8 ; i++ )
-            {
-                result += letters [ e.Next(0,letters.Length) ];//mismo comentario que linea 20, pero en este caso Length-1
-            }
-
-            return result;
-        }
-
-    }
-
-    public class GameActions
-    {
-        public static void GiveActions()
-       {
-           GameEngine.CantActionsPerTurn ++;
-       }
-
-       public static void SaveCards(int index) //index de la carta a salvar para el proximo turno
-       {
-           GameUtils.Move(TablePlayer.HandCards, TablePlayer.SavedCards, index);
-       }
-
-       public static void GetDeck(List<int> indexs)
-       {
-           while(indexs.Count>0)
-           {
-                GameUtils.Move(TablePlayer.Deck, TablePlayer.DiscardPile, indexs[indexs.Count-1]);
-                indexs.RemoveAt(indexs.Count-1);
-           }
-       }
-        public static void Attack(int index,IPlayer attack)
-        {
-            GameUtils.Move(TablePlayer.GemPile, attack.TablePlayer.GemPile, index);
-        }
-
-        public static void Trash(Bank bank, Card card, List<Card> list)
-        {
-            bank.Add(card);
-            list.Remove(card);
-        }
-
-        public static void GainCard(Bank bank, IPlayer a, Card card)
-        {
-            a.TablePlayer.DiscardPile.Add(bank.Get(card));
-        }
-    }
-
     public class GameEngine
     {
-        string Historial = "";
-        public int CantActionsPerTurn = 1;
-        int CantMoneyPerTurn = 0;
-        GameTurns? Turns ;
-        public void PlayGame(List<IPlayer> players,Bank bank)
+        public static string Historial = "";
+        public static int CantActionsPerTurn = 1;
+        public static int CantMoneyPerTurn = 0;
+        public static GameTurns Turns ;
+        public static Bank bank;
+        public static IPlayer PlayGame(List<IPlayer> players,Bank b)
         {
             Turns = new GameTurns(players);
+            bank = b;
             while(true)
             {
                 Turns.MoveNext();
                 ActionPhase(Turns.Current,bank);
                 BuyPhase(Turns.Current);
-                CleanUpPhase(Turns.Current);
+                CantActionsPerTurn = 1;
+                CantMoneyPerTurn = 0;
+
+                if( CleanUpPhase(Turns.Current) >= 10 )
+                {
+                    int cantmingems = int.MaxValue;
+                    int cantgems;
+                    List<IPlayer> winplayers=new List<IPlayer>();
+
+                    foreach(IPlayer a in players)
+                    {
+                        cantgems = a.Table.CantGem();
+
+                        if(cantgems<cantmingems)
+                        {
+                            cantmingems = cantgems;
+                            winplayers.Clear();
+                            winplayers.Add(a);
+                        }
+                        else if(cantgems == cantmingems) winplayers.Add(a);
+                    }
+                    if(winplayers.Count == 1)
+                    {
+                        return winplayers[0];
+                    }
+
+                    return PlayGame(winplayers,bank);
+                }
             }
         }
 
-        public void ActionPhase(IPlayer a,Bank bank)
+        public void ActionPhase(IPlayer a, Bank bank)
         {
-            int index = a.SelectCardHand();
-            a.TablePlayer.HandCards.RemoveAt(index);
-            GameUtils.Move(TablePlayer.HandCards, TablePlayer.OnGoing, index);
-            Card card = a.SelectCardOnGoing();
-            a.ChooseActionRealize(card,bank);
+            while(CantActionsPerTurn>0)
+            {
+                if(a.Table.OnGoing.Count!=0 && a.SelectField())
+                {
+                    Card card = a.SelectCardOnGoing();//seleccionando carta del ongoing
+                    a.ChooseActionRealize(card,bank);//escogiendo y ejecutando accion de la carta
+                }
+                else{
+                    a.SelectCardHand();//seleccionar una carta de la mano
+                    Card card = a.SelectCardOnGoing();//seleccionando carta del ongoing
+                    a.ChooseActionRealize(card,bank);//escogiendo y ejecutando accion de la carta
+                }
+                
+                CantActionsPerTurn--;
+            }
         }
 
-        public void BuyPhase(IPlayer a)
+        public static void BuyPhase(IPlayer a)
         {
+            if ( CantMoneyPerTurn <= 0 )
+            {
+                a.Table.ToDiscardPile((IEnumerable<Card>)bank.GetCant(new Cup(),1-CantActionsPerTurn));     
+            }
+            else
+            {
+                while( CantActionsPerTurn > 0 )
+                {
+                    ICostable BuyCard = a.PlayBuyPhase();
 
+                    if(BuyCard == null)
+                    {
+                        break;
+                    }
+
+                    if(CantMoneyPerTurn-BuyCard.Cost>=0)
+                    {
+                        BuyCard = bank.Get(BuyCard);
+                        if(BuyCard != null)
+                        {
+                            a.Table.ToOnGoing((Card) BuyCard);
+                            CantMoneyPerTurn -= BuyCard.Cost;
+                        }
+                    }
+                }
+            }
         }
 
-        public void CleanUpPhase(IPlayer a)
+        public static int CleanUpPhase(IPlayer a)
         {
-
+            a.Table.CleanUp();
+            return a.Table.CantGem();
         }
     }
 }
