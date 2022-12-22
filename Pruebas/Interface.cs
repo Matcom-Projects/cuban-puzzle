e264b2db00ca3ece9d86e7780757f0a9bd267201
Kpiro <106298160+Kpiro@users.noreply.github.com>
2022-12-22 17:05:18 -0500

    5:04pm 22/12 (se agregó la interface IActionable)

---
+++ Pruebas/Interface.cs
@@ -0,0 +1,37 @@
+﻿namespace AppConsole//Cambios 
+{
+    public interface ICostable
+    {
+        public int Cost{ get; }
+    }
+
+    public interface IPlayer
+    {
+        public string Name { get; }
+        public TablePlayer Table { get; set; }
+        
+        public int SelectHero(List<Card> a);
+        public int SelectActionCard(List<ICostable> a);
+        public void SelectCardHand();
+        public Card SelectCardOnGoing();
+        public bool SelectField();
+        public int SelectGem();
+        public IPlayer SelectPlayer(IPlayer a);
+        public void ChooseActionRealize(Card card, Bank bank);
+        public ICostable PlayBuyPhase();
+        public void PlayCleanUpPhase();
+        
+    }
+
+    public interface IActionable
+    {
+        public bool[] Actions {get{ this.Actions = new bool[6];}}
+
+        public void GiveActions();
+        public void SaveCards(int index);
+        public void ExecuteGetDeck();
+        public void Attack(int index,IPlayer a);
+        public void Trash(Card card);
+        public void GainCard(Card card);
+    }
+}
