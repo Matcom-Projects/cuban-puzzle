2a772ece4e502881b6944614ddb2069e97577f4a
Kpiro <106298160+Kpiro@users.noreply.github.com>
2022-12-23 17:17:10 -0500

    Add files via upload

---
+++ Pruebas/AllChips/Heroes/DrFranciscoDuran/MartialMastery.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+using System.Linq;
+namespace AppConsole
+{
+    public class MartialMastery : Card, IActionable
+    {
+        public bool[] Actions {get; set;}
+        public string Id{ get; private set; }
+        public MartialMastery() : base("Martial Mastery", new string[]{"yellow"}, 0)
+        {
+            this.Actions = new bool[] {true, false, false, false, true, false};
+            this.Id = GameUtils.CreateId();
+        }
+        private Card gain {get; set;}
+        public void GiveActions()
+        {
+            GameEngine.CantActionsPerTurn ++;
+        }
+
+        private void SaveCards(int index);
+        private void ExecuteGetDeck(IPlayer a);
+        private void Attack(int index,IPlayer a);
+
+        public void Trash(Bank bank, IPlayer a)
+        {
+            do{
+                Card card = a.Table.HandCards[a.SelectCardHand()];
+            }while(card.Color.Contains("purple"));
+            
+            bank.Add(card);
+            a.Table.HandCards.Remove(card);
+            Actions[5] = true;
+            gain = card;
+            GameEngine.CantActionsPerTurn ++;
+        }
+
+        public void GainCard(Bank bank, IPlayer a)
+        {
+            List<Card> list = new List<Card>();
+            foreach(var key in bank.GameBank.Keys)
+            {
+                if(key.Cost == (gain.Cost+2)) list.Add(key);
+            }
+            a.Table.DiscardPile.Add(bank.Get(a.SelectCardBank(list)));
+        }
+
+        /*Informacion de la carta:
+        1. Da una accion mas
+        2. Trashea una carta de su mano que no sea morada: (Da una accion mas y activa accion 2.1)
+         2.1 Gana una carta que tenga costo exactamente 2 mas que la carta trasheada
+        */
+    }
+}
