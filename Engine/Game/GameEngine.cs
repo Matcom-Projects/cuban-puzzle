3611a1eb2457943dc2469f8656ae7fa558d8baa3
Kpiro <106298160+Kpiro@users.noreply.github.com>
2022-12-26 05:13:40 -0500

    26/12 5:13am

--- Engine/Game/GameEngine.cs
+++ Engine/Game/GameEngine.cs
@@ -2,120 +2,118 @@ namespace engine_cuban_puzzle;
 using System.Collections.Generic;
 
 public static class GameEngine
-{
-    public static string Historial = "";
-    public static int CantActionsPerTurn = 1;
-    public static int CantMoneyPerTurn = 0;
-    public static List<IPlayer> Players;
-    public static GameTurns Turns;
-    public static Bank bank;
-
-    public static IPlayer PlayGame(List<IPlayer> players,Bank b)
     {
-        Players = players;
-        Turns = new GameTurns(players);
-        bank = b;
-        while(true)
+        public static string Historial = "";
+        public static int CantActionsPerTurn = 1;
+        public static int CantMoneyPerTurn = 0;
+        public static List<IPlayer> Players;
+        public static GameTurns Turns;
+        public static Bank bank;
+
+        public static IPlayer PlayGame(List<IPlayer> players,Bank b)
         {
-            Turns.MoveNext();
-            Turns.Current.Table.ToGemPile(bank.Get(0));
-            ActionPhase(Turns.Current);
-            BuyPhase(Turns.Current);
-            CantActionsPerTurn = 1;
-            CantMoneyPerTurn = 0;
-
-            if( CleanUpPhase(Turns.Current) >= 10 )
+            Players = players;
+            Turns = new GameTurns(players);
+            bank = b;
+            while(true)
             {
-                int cantmingems = int.MaxValue;
-                int cantgems;
-                List<IPlayer> winplayers=new List<IPlayer>();
+                Turns.MoveNext();
+                Turns.Current.Table.ToGemPile(bank.Get(0));
+                ActionPhase(Turns.Current);
+                BuyPhase(Turns.Current);
+                CantActionsPerTurn = 1;
+                CantMoneyPerTurn = 0;
 
-                foreach(IPlayer a in players)
+                if( CleanUpPhase(Turns.Current) >= 10 )
                 {
-                    cantgems = a.Table.CantGem();
+                    int cantmingems = int.MaxValue;
+                    int cantgems;
+                    List<IPlayer> winplayers=new List<IPlayer>();
 
-                    if(cantgems<cantmingems)
+                    foreach(IPlayer a in players)
                     {
-                        cantmingems = cantgems;
-                        winplayers.Clear();
-                        winplayers.Add(a);
-                    }
-                    else if(cantgems == cantmingems) winplayers.Add(a);
-                }
-                if(winplayers.Count == 1)
-                {
-                    return winplayers[0];
-                }
+                        cantgems = a.Table.CantGem();
 
-                return PlayGame(winplayers,bank);
+                        if(cantgems<cantmingems)
+                        {
+                            cantmingems = cantgems;
+                            winplayers.Clear();
+                            winplayers.Add(a);
+                        }
+                        else if(cantgems == cantmingems) winplayers.Add(a);
+                    }
+                    if(winplayers.Count == 1)
+                    {
+                        return winplayers[0];
+                    }
+
+                    return PlayGame(winplayers,bank);
+                }
             }
         }
-    }
 
-    public static void ActionPhase(IPlayer a)
-    {
-        while( CantActionsPerTurn > 0 )
+        public static void ActionPhase(IPlayer a)
         {
-            GamePrint.PrintTable(Players);
-            if(a.Exit()) return;
-            else{
+            while( CantActionsPerTurn > 0 )
+            {
                 GamePrint.PrintTable(Players);
-                if(a.Table.OnGoing.Count!=0 && a.SelectField())
-                {
-                    GamePrint.PrintTable(Players);
-                    Card card = a.SelectCardOnGoing();//seleccionando carta del ongoing
-                    GamePrint.PrintTable(Players);
-                    a.ChooseActionRealize((IActionable)card);//escogiendo y ejecutando accion de la carta
-                }
+                if(a.Exit()) return;
                 else{
                     GamePrint.PrintTable(Players);
-                    a.Table.HandToOnGoing(a.SelectCardHand());//moverla hacia el ongoing    
-                    GamePrint.PrintTable(Players);        
-                    Card card = a.SelectCardOnGoing();//seleccionando carta del ongoing
-                    GamePrint.PrintTable(Players);
-                    a.ChooseActionRealize((IActionable)card);//escogiendo y ejecutando accion de la carta
-                }
-            }
-            GamePrint.PrintTable(Players);
-            CantActionsPerTurn--;
-        }
-    }
-
-    public static void BuyPhase(IPlayer a)
-    {
-        CantMoneyPerTurn += a.Table.CantMoneyBuyPhases();
-        if ( CantMoneyPerTurn < 0 )
-        {
-            a.Table.ToDiscardPile(bank.GetCant(bank.keys[7],1-CantActionsPerTurn));     
-        }
-        else
-        {
-            while( CantMoneyPerTurn >= 0 )
-            {
-                BankCard BuyCard = a.PlayBuyPhase();
-
-                if(BuyCard == null)
-                {
-                    continue;
-                }
-
-                if(CantMoneyPerTurn-BuyCard.Cost>=0)
-                {
-                    BuyCard = bank.Get(BuyCard);
-                    if(BuyCard != null)
+                    if(a.Table.OnGoing.Count!=0 && a.SelectField())
                     {
-                        a.Table.ToOnGoing((Card) BuyCard);
-                        CantMoneyPerTurn -= BuyCard.Cost;
+                        Card card = a.SelectCardOnGoing();//seleccionando carta del ongoing
+                        GamePrint.PrintTable(Players);
+                        a.ChooseActionRealize((IActionable)card);//escogiendo y ejecutando accion de la carta
+                    }
+                    else{
+                        a.Table.HandToOnGoing(a.SelectCardHand());//moverla hacia el ongoing    
+                        GamePrint.PrintTable(Players);        
+                        Card card = a.SelectCardOnGoing();//seleccionando carta del ongoing
+                        GamePrint.PrintTable(Players);
+                        a.ChooseActionRealize((IActionable)card);//escogiendo y ejecutando accion de la carta
                     }
                 }
-
-                if(a.PlayNextBuyPhases()) break;
+    
+                CantActionsPerTurn--;
             }
         }
-    }
 
-    public static int CleanUpPhase(IPlayer a)
-    {
-        return a.Table.CleanUp();
+        public static void BuyPhase(IPlayer a)
+        {
+            CantMoneyPerTurn += a.Table.CantMoneyBuyPhases();
+            if ( CantMoneyPerTurn < 0 )
+            {
+                a.Table.ToDiscardPile(bank.GetCant(bank.keys[7],1-CantActionsPerTurn));     
+            }
+            else
+            {
+                while( CantMoneyPerTurn >= 0 )
+                {
+                    BankCard BuyCard = a.PlayBuyPhase();
+
+                    if(BuyCard == null)
+                    {
+                        continue;
+                    }
+
+                    if(CantMoneyPerTurn-BuyCard.Cost>=0)
+                    {
+                        BuyCard = bank.Get(BuyCard);
+                        if(BuyCard != null)
+                        {
+                            a.Table.ToOnGoing((Card) BuyCard);
+                            CantMoneyPerTurn -= BuyCard.Cost;
+                        }
+                    }
+
+                    if(a.PlayNextBuyPhases()) break;
+                }
+            }
+        }
+
+        public static int CleanUpPhase(IPlayer a)
+        {
+            return a.Table.CleanUp();
+        }
     }
-}
