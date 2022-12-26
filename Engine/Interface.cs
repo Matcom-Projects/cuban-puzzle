25de516e4afba39f350808f475413cc82091db3a
Kpiro <106298160+Kpiro@users.noreply.github.com>
2022-12-26 05:18:41 -0500

    26/12 5:18am

--- Engine/Interface.cs
+++ Engine/Interface.cs
@@ -1,31 +1,30 @@
 namespace engine_cuban_puzzle;
 
 public interface IPlayer
-{
-    public string Name { get; }
-    public TablePlayer Table { get; set; }
-    
-        public int SelectActionCard(List<BankCard> a);
-        public int SelectHero(List<Card> a);
-        public int SelectCardHand();
-        public Card SelectCardOnGoing();
-        public bool SelectField();
-        public int SelectGem();
-        public IPlayer SelectPlayer(IPlayer a);
-        public bool Exit();//este metodo puede que sirva o puede q no
-        public void ChooseActionRealize(IActionable card);
-        public BankCard SelectCardBank(List<BankCard> list);//este metodo quitarlo desp
-        public int SelectCardDeck();//este metodo quitarlo desp
-        public BankCard PlayBuyPhase();
-        public bool PlayNextBuyPhases();
-}
-public interface IActionable
-{
-    public bool[] Actions { get; }
-    public void GiveActions();
-    public void SaveCards(int index, IPlayer a);
-    public void Draw(IPlayer a);
-    public void Attack(int index,IPlayer a);
-    public void Trash(IPlayer a);
-    public void GainCard(IPlayer a);
-}
+    {
+        public string Name { get; }
+        public TablePlayer Table { get; set; }
+        
+            public bool Exit();
+            public int SelectCardHand();
+            public Card SelectCardOnGoing();
+            public bool SelectField();
+            public void ChooseActionRealize(IActionable card);
+            public int SelectGem();
+            public IPlayer SelectPlayer();
+            public BankCard SelectCardBank(List<BankCard> list);//este metodo quitarlo desp
+            public int SelectCardDeck();//este metodo quitarlo desp
+            public BankCard PlayBuyPhase();
+            public bool PlayNextBuyPhases();
+            
+    }
+    public interface IActionable
+    {
+        public bool[] Actions { get; }
+        public void GiveActions();
+        public void SaveCards(int index, IPlayer a);
+        public void Draw(IPlayer a);
+        public void Attack(int index,IPlayer a);
+        public void Trash(IPlayer a);
+        public void GainCard(IPlayer a);
+    }
