d657fc91db84585588260df5b8581516fb439f05
Kpiro <106298160+Kpiro@users.noreply.github.com>
2022-12-29 03:16:46 -0500

    29/12 (se le quito el private al Money)

--- Engine/Cards/ClassCards.cs
+++ Engine/Cards/ClassCards.cs
@@ -6,7 +6,7 @@ public abstract class Card
     public string Id { get; private set; }
     public string Name { get; private set; }
     public string[] Color { get; private set; }
-    public int Money { get; private set; }
+    public int Money { get; set; }
     public Card ( string name,string[]color,int money )
     {
         this.Id = GameUtils.CreateId();
