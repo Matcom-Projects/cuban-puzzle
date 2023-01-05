a4fe232c165136eadcb7beb72380c5351aba6a00
Kpiro <106298160+Kpiro@users.noreply.github.com>
2023-01-05 15:53:14 -0500

    cambios en las cartas

--- Engine/Cards/PlayingCard/One-TwoPunch.cs
+++ Engine/Cards/PlayingCard/One-TwoPunch.cs
@@ -1,29 +1,24 @@
 namespace engine_cuban_puzzle;
 
-public class OneTwoPunch : BankCard, IActionable
-{
-    public bool[] Actions {get; set;}
-    public OneTwoPunch() : base("One-Two Punch", new string[]{"yellow"}, 0,4)
+public class OneTwoPunch : ActionBankCard
     {
-        this.Actions = new bool[] {true, false, false, false, false, false};
-    }
+        public OneTwoPunch() : base("One-Two Punch", new string[]{"yellow"}, 0,4)
+        {
+            
+        }
 
-    public void GiveActions()
-    {
-        GameEngine.CantActionsPerTurn += 2;
-    }
-    public void SaveCards(int index, IPlayer a){}
-    public void Draw(IPlayer a){}
-    public void Attack(int index,IPlayer a){}
-    public void Trash(IPlayer a){}
-    public void GainCard(IPlayer a){}
-    public override string ToString()
-    {
-        Console.ForegroundColor = ConsoleColor.Yellow;
-        return $"[{this.Name}]";
-    }
+        public override void Action(IPlayer a)
+        {
+            GameEngine.CantActionsPerTurn += 2;
+        }
 
-    /*Informacion de la carta:
-    1. Da dos acciones mas
-    */
-}
+        public override string ToString()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            return $"[{this.Name}]";
+        }
+
+        /*Informacion de la carta:
+        1. Da dos acciones mas
+        */
+    }
