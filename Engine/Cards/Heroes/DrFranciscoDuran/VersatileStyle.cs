0ccfed7827fb482323468d81677b39f95dda43e6
Kpiro <106298160+Kpiro@users.noreply.github.com>
2023-01-05 14:58:30 -0500

    cambios en las cartas

--- Engine/Cards/Heroes/DrFranciscoDuran/VersatileStyle.cs
+++ Engine/Cards/Heroes/DrFranciscoDuran/VersatileStyle.cs
@@ -1,32 +1,21 @@
 namespace engine_cuban_puzzle;
 
-public class VersatileStyle : Card, IActionable
-{
-    public bool[] Actions {get; set;}
-    public VersatileStyle() : base("Versatile Style", new string[]{"yellow"}, 2)
+public class VersatileStyle : ActionCard
     {
-        this.Actions = new bool[] {true, true, true, false, false, false};
-    }
-    public void GiveActions()
-    {
-        GameEngine.CantActionsPerTurn ++;
-    }
+        public VersatileStyle() : base("Versatile Style", new string[]{"yellow"}, 2)
+        {
+            
+        }
+        public override void Action(IPlayer a)
+        {
+            GameActions.GiveActions();
+            GameActions.SaveCards(a.SelectCardHand(),a);
+            GameActions.Draw(2,a);
+        }
 
-    public void SaveCards(int index, IPlayer a)
-    {
-        a.Table.HandToSaveCards(index);
+        /*Informacion de la carta:
+        1. Da una accion mas y 2 pesos mas para la fase de compra
+        2. Guarda una carta para el proximo turno
+        3. Coge dos cartas del deck
+            */
     }
-    public void Draw(IPlayer a)
-    {
-        a.Table.DrawDeck(2);
-    }
-    public void Attack(int index,IPlayer a){}
-    public void Trash(IPlayer a){}
-    public void GainCard(IPlayer a){}
-
-    /*Informacion de la carta:
-    1. Da una accion mas y 2 pesos mas para la fase de compra
-    2. Guarda una carta para el proximo turno
-    3. Coge dos cartas del deck
-        */
-}
