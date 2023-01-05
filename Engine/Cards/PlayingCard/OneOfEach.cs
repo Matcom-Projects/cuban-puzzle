ce1297a168be927d587cae8ccf1de7ffd506d936
Kpiro <106298160+Kpiro@users.noreply.github.com>
2023-01-05 15:54:08 -0500

    cambios en las cartas

--- Engine/Cards/PlayingCard/OneOfEach.cs
+++ Engine/Cards/PlayingCard/OneOfEach.cs
@@ -1,38 +1,28 @@
 namespace engine_cuban_puzzle;
 
-public class OneOfEach : BankCard, IActionable
-{
-    public bool[] Actions {get; set;}
-    public OneOfEach() : base("One Of Each", new string[]{"yellow"}, 1,5)
+public class OneOfEach : ActionBankCard
     {
-        this.Actions = new bool[] {true, true, true, false, false, false};
-    }
+        public OneOfEach() : base("One Of Each", new string[]{"yellow"}, 1,5)
+        {
+            
+        }
 
-    public void GiveActions()
-    {
-        GameEngine.CantActionsPerTurn ++;
-    }
-    public void SaveCards(int index, IPlayer a)
-    {
-        a.Table.HandToSaveCards(index);
-    }
-    public void Attack(int index,IPlayer a){}
-    public void Trash(IPlayer a){}
-    public void GainCard(IPlayer a){}
+        public override void Action(IPlayer a)
+        {
+            GameActions.GiveActions();
+            GameActions.SaveCards(a.SelectCardHand(), a);
+            GameActions.Draw(1,a);
+        }
 
-    public void Draw(IPlayer a)
-    {
-        a.Table.DrawDeck(1);
-    }
-    public override string ToString()
-    {
-        Console.ForegroundColor = ConsoleColor.Yellow;
-        return $"[{this.Name}]";
-    }
+        public override string ToString()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            return $"[{this.Name}]";
+        }
 
-    /*Informacion de la carta:
-    1. Da una accion mas y 1$ para la fase de compra
-    2. Guarda una carta para el proximo turno
-    3. Coge una carta del deck
-    */
-}
+        /*Informacion de la carta:
+        1. Da una accion mas y 1$ para la fase de compra
+        2. Guarda una carta para el proximo turno
+        3. Coge una carta del deck
+        */
+    }
