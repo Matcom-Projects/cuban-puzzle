89604cd28e2bb4b080e6356c9b18cf8280d2b7ed
Kpiro <106298160+Kpiro@users.noreply.github.com>
2023-01-05 15:00:15 -0500

    cambios en las cartas

--- Engine/Cards/Heroes/EdithMassola/PlayingWithFire.cs
+++ Engine/Cards/Heroes/EdithMassola/PlayingWithFire.cs
@@ -1,28 +1,21 @@
 namespace engine_cuban_puzzle;
 
-public class PlayingWithFire : Card, IActionable
-{
-    public bool[] Actions {get; set;}
-    public PlayingWithFire() : base("Playing With Fire", new string[]{"yellow"}, 0)
+public class PlayingWithFire : ActionCard
     {
-        this.Actions = new bool[] {true, false, true, false, false, false};
-    }
+        public PlayingWithFire() : base("Playing With Fire", new string[]{"yellow"}, 0)
+        {
 
-    public void GiveActions()
-    {
-        GameEngine.CantActionsPerTurn += 2;
-    }
-    public void SaveCards(int index, IPlayer a){}
-    public void Draw(IPlayer a)
-    {
-        a.Table.DrawDeck(1);
-    }
-    public void Attack(int index,IPlayer a){}
-    public void Trash(IPlayer a){}
-    public void GainCard(IPlayer a){}
+        }
 
-    /*Informacion de la carta:
-    1. Da dos acciones mas
-    2. Coge una carta del deck 
-    */
-}
+        public override void Action(IPlayer a)
+        {
+            GameEngine.CantActionsPerTurn += 2;
+            GameActions.Draw(1,a);
+        }
+        
+
+        /*Informacion de la carta:
+        1. Da dos acciones mas
+        2. Coge una carta del deck 
+        */
+    }
