cb048851702b20ccd861d9c0787659709b6037f5
Kpiro <106298160+Kpiro@users.noreply.github.com>
2023-01-05 15:10:17 -0500

    cambios en las cartas

--- Engine/Cards/PlayingCard/GemEssence.cs
+++ Engine/Cards/PlayingCard/GemEssence.cs
@@ -1,35 +1,29 @@
 namespace engine_cuban_puzzle;
 
-public class GemEssence : BankCard, IActionable
-{
-    public bool[] Actions {get; set;}
-    public GemEssence() : base("Gem Essence", new string[]{"yellow"}, 0,3)
+public class GemEssence : ActionBankCard
     {
-        this.Actions = new bool[] {false, false, false, false, true, false};
-    }
-
-    public void GiveActions(){}
-    public void SaveCards(int index, IPlayer a){}
-    public void Draw(IPlayer a){}
-    public void Attack(int index,IPlayer a){}
-    public void Trash(IPlayer a)
-    {
-        if(a.Table.HandCards.Contains(new Gem1()))
+        public GemEssence() : base("Gem Essence", new string[]{"yellow"}, 0,3)
         {
-            BankCard card = new Gem1();
-            GameEngine.bank.Add(card);
-            a.Table.HandCards.Remove(card);
-            GameEngine.CantActionsPerTurn++;
-        }
-    }
-    public void GainCard(IPlayer a){}
-    public override string ToString()
-    {
-        Console.ForegroundColor = ConsoleColor.Yellow;
-        return $"[{this.Name}]";
-    }
 
-    /*Informacion de la carta:
-    1. Trashea una gema de 1 y tienes una accion mas
-    */
-}
+        }
+
+        public override void Action(IPlayer a)
+        {
+            if(a.Table.HandCards.Contains(GameEngine.bank.keys[0]))
+            {
+                int i = a.Table.HandCards.IndexOf(GameEngine.bank.keys[0]);
+                GameActions.Trash(i,a.Table.HandCards);
+                GameActions.GiveActions();
+            }
+        }
+        
+        public override string ToString()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            return $"[{this.Name}]";
+        }
+
+        /*Informacion de la carta:
+        1. Trashea una gema de 1 y tienes una accion mas
+        */
+    }
