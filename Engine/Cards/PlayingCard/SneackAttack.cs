c37c552c431669835d5b95a3a4dfc8440360de85
Kpiro <106298160+Kpiro@users.noreply.github.com>
2023-01-05 15:57:34 -0500

    cambios en las cartas

--- Engine/Cards/PlayingCard/SneackAttack.cs
+++ Engine/Cards/PlayingCard/SneackAttack.cs
@@ -1,55 +1,31 @@
 namespace engine_cuban_puzzle;
 
-public class SneackAttack : BankCard, IActionable
-{
-    public bool[] Actions {get; set;}
-    public SneackAttack() : base("Sneack Attack", new string[]{"red"}, 0,3)
+public class SneackAttack : ActionBankCard
     {
-        this.Actions = new bool[] {true, false, false, false, false, true};
-    }
-
-    public void GiveActions()
-    {
-        GameEngine.CantActionsPerTurn ++;
-    }
-
-    public void GainCard(IPlayer a)
-    {
-        a.Table.DiscardPile.Add((Card)GameEngine.bank.Get(0));            
-        foreach(var p in GameEngine.Turns.Players)
+        public SneackAttack() : base("Sneack Attack", new string[]{"red"}, 0,3)
         {
-            if(p==a) continue;
-            p.Table.GemPile.Add(GameEngine.bank.Get(0));
+            
         }
-    }
 
-    void IActionable.SaveCards(int index, IPlayer a)
-    {
-        throw new NotImplementedException();
-    }
+        public override void Action(IPlayer a)
+        {
+            GameActions.GiveActions();
+            GameActions.GainCard(a, GameEngine.bank.keys[0]);
+            foreach(var p in GameEngine.Turns.Players)
+            {
+                if(p==a) continue;
+                p.Table.GemPile.Add(GameEngine.bank.Get(GameEngine.bank.keys[0]));
+            }
+        }
 
-    void IActionable.Draw(IPlayer a)
-    {
-        throw new NotImplementedException();
-    }
+        public override string ToString()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            return $"[{this.Name}]";
+        }
 
-    void IActionable.Attack(int index, IPlayer a)
-    {
-        throw new NotImplementedException();
+        /*Informacion de la carta:
+        1. Da una accion mas
+        2. Gana un gem1 y pone un gem1 en las pilas de gemas de los oponentes
+        */
     }
-
-    void IActionable.Trash(IPlayer a)
-    {
-        throw new NotImplementedException();
-    }
-    public override string ToString()
-    {
-        Console.ForegroundColor = ConsoleColor.Red;
-        return $"[{this.Name}]";
-    }
-
-    /*Informacion de la carta:
-    1. Da una accion mas
-    2. Gana un gem1 y pone un gem1 en las pilas de gemas de los oponentes
-    */
-}
