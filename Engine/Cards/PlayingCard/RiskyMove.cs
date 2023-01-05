5212f12e8df6fb730083fc3e32a1f81bd43a44bb
Kpiro <106298160+Kpiro@users.noreply.github.com>
2023-01-05 15:55:07 -0500

    cambios en las cartas

--- Engine/Cards/PlayingCard/RiskyMove.cs
+++ Engine/Cards/PlayingCard/RiskyMove.cs
@@ -1,33 +1,20 @@
 namespace engine_cuban_puzzle;
 
-// public class RiskyMove : BankCard, IActionable
-// {
-//     public bool[] Actions {get; set;}
-//     public RiskyMove() : base("Risky Move", new string[]{"yellow"}, 0,1)
-//     {
-//         this.Actions = new bool[] {false, false, false, false, false, true};
-//     }
+public class RiskyMove : ActionBankCard
+    {
+        public RiskyMove() : base("Risky Move", new string[]{"yellow"}, 0,1)
+        {
+            
+        }
 
-//     public void GiveActions(){}
-//     public void SaveCards(int index, IPlayer a){}
-//     public void Draw(IPlayer a){}
-//     public void Attack(int index,IPlayer a){}
-//     public void Trash(IPlayer a){}
-//     public void GainCard(IPlayer a)
-//     {
-//         for(int i=0; i<a.Table.HandCards.Count; i++)
-//         {
-//             if(a.Table.HandCards[i] is Gem1)
-//             {
-//                 GameUtils.Move(a.Table.HandCards, a.Table.GemPile, i);
-//                 a.Table.DiscardPile.Add(GameEngine.bank.Get(new Gem2()));
-//                 GameEngine.CantMoneyPerTurn += 3;
-//                 return;
-//             }
-//         }
-//     }
+        public override void Action(IPlayer a)
+        {
+            GameActions.GainCard(a, GameEngine.bank.keys[1]);
+            GameEngine.CantMoneyPerTurn += 3;
+        }
 
-//     /*Informacion de la carta:
-//     1. Pon una gem1 de tu mano en tu gema de pila, entonces ganas una gem2 y 3$ para la fase de compra
-//     */
-// }
+
+        /*Informacion de la carta:
+        1. Ganas una gem2 y 3$ para la fase de compra
+        */
+    }
