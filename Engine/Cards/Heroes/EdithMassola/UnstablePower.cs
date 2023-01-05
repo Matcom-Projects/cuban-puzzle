db0d222071e0b3b9487a54db60a94b449f2c1d6e
Kpiro <106298160+Kpiro@users.noreply.github.com>
2023-01-05 15:01:06 -0500

    cambios en las cartas

--- Engine/Cards/Heroes/EdithMassola/UnstablePower.cs
+++ Engine/Cards/Heroes/EdithMassola/UnstablePower.cs
@@ -1,25 +1,21 @@
 namespace engine_cuban_puzzle;
 
-public class UnstablePower : Card, IActionable
-{
-    public bool[] Actions {get; set;}
-    public UnstablePower() : base("Unstable Power", new string[]{"purple"}, 0)
+public class UnstablePower : ActionCard
     {
-        this.Actions = new bool[] {false, false, false, false, false, true};
-    }
+        public UnstablePower() : base("Unstable Power", new string[]{"purple"}, 0)
+        {
+            
+        }
 
-    public void GiveActions(){}
-    public void SaveCards(int index, IPlayer a){}
-    public void Draw(IPlayer a){}
-    public void Attack(int index,IPlayer a){}
-    public void Trash(IPlayer a){}
-    public void GainCard(IPlayer a)
-    {
-        a.Table.DiscardPile.Add(GameEngine.bank.Get(5));
-        a.Table.DiscardPile.AddRange(GameEngine.bank.GetCant(7,2));
-    }
+        public override void Action(IPlayer a)
+        {
+            GameActions.GainCard(a, GameEngine.bank.keys[5]);
+            GameActions.GainCard(a, GameEngine.bank.keys[7]);
+            GameActions.GainCard(a, GameEngine.bank.keys[7]);
+        }
+        
 
-    /*Informacion de la carta:
-    1. Gana un doble crash gem, pero tambien dos CUP 
-    */
-}
+        /*Informacion de la carta:
+        1. Gana un doble crash gem, pero tambien dos CUP 
+        */
+    }
