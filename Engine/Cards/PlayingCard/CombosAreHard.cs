90344b3e3de7d7121ed25c8d9d5a840f69066745
Kpiro <106298160+Kpiro@users.noreply.github.com>
2023-01-05 15:09:00 -0500

    cambios en las cartas

--- Engine/Cards/PlayingCard/CombosAreHard.cs
+++ Engine/Cards/PlayingCard/CombosAreHard.cs
@@ -1,41 +1,27 @@
 namespace engine_cuban_puzzle;
 
-public class CombosAreHard : BankCard, IActionable
-{
-    public bool[] Actions {get; set;}
-    public CombosAreHard() : base("Combos Are Hard", new string[]{"yellow"}, 0,6)
+public class CombosAreHard : ActionBankCard
     {
-        this.Actions = new bool[] {false, false, false, false, false, true};
-    }
-
-    public void GiveActions(){}
-    public void SaveCards(int index, IPlayer a){}
-    public void Draw(IPlayer a){}
-    public void Attack(int index,IPlayer a){}
-    public void Trash(IPlayer a){}
-    public void GainCard(IPlayer a)
-    {
-        for(int i=0; i < 2; i++)
+        public CombosAreHard() : base("Combos Are Hard", new string[]{"yellow"}, 0,6)
         {
-            List<BankCard> list = new List<BankCard>();
-            foreach(var key in GameEngine.bank.keys)
-            {
-                list.Add(key);
-            }
-            
-            a.Table.DiscardPile.Add(GameEngine.bank.Get(list[GamePrint.SelectCard(list)]));
+    
         }
 
-        GameEngine.bank.Add(this);
-        a.Table.OnGoing.Remove(this);
-    }
-    public override string ToString()
-    {
-        Console.ForegroundColor = ConsoleColor.Yellow;
-        return $"[{this.Name}]";
-    }
+        public override void Action(IPlayer a)
+        {
+            GameActions.GainCard(a,GameEngine.bank.keys[3]);
+            GameActions.GainCard(a,GameEngine.bank.keys[3]);
+            int index = a.Table.OnGoing.IndexOf(this);
+            GameActions.Trash(index, a.Table.OnGoing);
+        }
+        
+        public override string ToString()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            return $"[{this.Name}]";
+        }
 
-    /*Informacion de la carta:
-    1. Gana dos cartas y despues trashea esta carta 
-    */
-}
+        /*Informacion de la carta:
+        1. Gana dos Gem4 y despues trashea esta carta 
+        */
+    }
