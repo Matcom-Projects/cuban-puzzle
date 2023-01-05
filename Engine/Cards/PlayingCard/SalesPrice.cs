08d0f8cad8bd3073fe0e1a0c66daeb07f8ba9ad9
Kpiro <106298160+Kpiro@users.noreply.github.com>
2023-01-05 15:56:25 -0500

    cambios en las cartas

--- Engine/Cards/PlayingCard/SalesPrice.cs
+++ Engine/Cards/PlayingCard/SalesPrice.cs
@@ -1,29 +1,25 @@
 namespace engine_cuban_puzzle;
 
-public class SalesPrice : BankCard, IActionable
-{
-    public bool[] Actions {get; set;}
-    public SalesPrice() : base("Sales Price", new string[]{"yellow"}, 3,2)
+public class SalesPrice : ActionBankCard
     {
-        this.Actions = new bool[] {false, false, false, false, false, true};
-    }
+        public SalesPrice() : base("Sales Price", new string[]{"yellow"}, 3,2)
+        {
+            
+        }
 
-    public void GiveActions(){}
-    public void SaveCards(int index, IPlayer a){}
-    public void Draw(IPlayer a){}
-    public void Attack(int index,IPlayer a){}
-    public void Trash(IPlayer a){}
-    public void GainCard(IPlayer a)
-    {
-        a.Table.DiscardPile.Add((Card)GameEngine.bank.Get(0));
-    }
-    public override string ToString()
-    {
-        Console.ForegroundColor = ConsoleColor.Yellow;
-        return $"[{this.Name}]";
-    }
+        public override void Action(IPlayer a)
+        {
+            GameActions.GainCard(a, GameEngine.bank.keys[0]);
+            GameEngine.CantMoneyPerTurn += 3;
+        }
+        
+        public override string ToString()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            return $"[{this.Name}]";
+        }
 
-    /*Informacion de la carta:
-    1. Gana un gem1 y 3$ para la fase de compra
-    */
-}
+        /*Informacion de la carta:
+        1. Gana un gem1 y 3$ para la fase de compra
+        */
+    }
