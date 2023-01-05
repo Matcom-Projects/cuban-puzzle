3ff8604c54d3e952b524640c2fc4fc3ac9e5dd75
Kpiro <106298160+Kpiro@users.noreply.github.com>
2023-01-05 15:09:43 -0500

    cambios en las cartas

--- Engine/Cards/PlayingCard/DrawThree.cs
+++ Engine/Cards/PlayingCard/DrawThree.cs
@@ -1,30 +1,24 @@
 namespace engine_cuban_puzzle;
 
-public class DrawThree : BankCard, IActionable
-{
-    public bool[] Actions {get; set;}
-    public DrawThree() : base("Draw Three", new string[]{"yellow"}, 0,3)
+public class DrawThree : ActionBankCard
     {
-        this.Actions = new bool[] {false, false, true, false, false, false};
-    }
-    public void Action(){}
+        public DrawThree() : base("Draw Three", new string[]{"yellow"}, 0,3)
+        {
+            
+        }
 
-    public void GiveActions(){}
-    public void SaveCards(int index, IPlayer a){}
-    public void Draw(IPlayer a)
-    {
-        a.Table.DrawDeck(3);
-    }
-    public void Attack(int index,IPlayer a){}
-    public void Trash(IPlayer a){}
-    public void GainCard(IPlayer a){}
-    public override string ToString()
-    {
-        Console.ForegroundColor = ConsoleColor.Yellow;
-        return $"[{this.Name}]";
-    }
+        public override void Action(IPlayer a)
+        {
+            GameActions.Draw(3,a);
+        }
+        
+        public override string ToString()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            return $"[{this.Name}]";
+        }
 
-    /*Informacion de la carta:
-    1. Coge tres cartas del deck
-    */
-}
+        /*Informacion de la carta:
+        1. Coge tres cartas del deck
+        */
+    }
