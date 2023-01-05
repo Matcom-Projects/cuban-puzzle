f48e70c4162a9a7afab8556150ffe7213c35b906
Kpiro <106298160+Kpiro@users.noreply.github.com>
2023-01-05 14:57:36 -0500

    cambios en las cartas

--- Engine/Cards/Heroes/DrFranciscoDuran/Reversal.cs
+++ Engine/Cards/Heroes/DrFranciscoDuran/Reversal.cs
@@ -1,23 +1,18 @@
 namespace engine_cuban_puzzle;
 
-public class Reversal : Card, IActionable
-{
-    public bool[] Actions {get; set;}
-    public Reversal() : base("Reversal", new string[]{"purple"}, 0)
+public class Reversal : ActionCard
     {
-        this.Actions = new bool[] {false, false, true, false, false, false};
-    }
-    public void GiveActions(){}
-    public void SaveCards(int index, IPlayer a){}
-    public void Draw(IPlayer a)
-    {
-        a.Table.DrawDeck(2);
-    }
-    public void Attack(int index,IPlayer a){}
-    public void Trash(IPlayer a){}
-    public void GainCard(IPlayer a){}
+        public Reversal() : base("Reversal", new string[]{"purple"}, 0)
+        {
+            
+        }
+        public override void Action(IPlayer a)
+        {
+            GameActions.Draw(2,a);
+        }
 
-    /*Informacion de la carta:
-    1. Coge dos cartas del deck
-    */
-}
+
+        /*Informacion de la carta:
+        1. Coge dos cartas del deck
+        */
+    }
