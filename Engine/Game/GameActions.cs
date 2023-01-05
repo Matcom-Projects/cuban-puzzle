793bb1b9220aa23517c3cdf62a66b88b61a953e4
Kpiro <106298160+Kpiro@users.noreply.github.com>
2023-01-05 16:46:39 -0500

    modificacion del metodo attack

--- Engine/Game/GameActions.cs
+++ Engine/Game/GameActions.cs
@@ -1,56 +1,64 @@
 namespace engine_cuban_puzzle;
 
 public class GameActions
-{
-    public static void Move ( List<Card> a , List<Card> b , int index )
     {
-        b.Add(a[index]);
-        a.RemoveAt(index);
-    }
-
-    public static void GiveActions()
-    {
-        GameEngine.CantActionsPerTurn ++;
-    }
-
-    public static void Draw(int n,IPlayer Player)
-    {
-        Player.Table.DrawDeck(n);
-    }
-
-    public static void SaveCards(int index,IPlayer Player)
-    {
-        Player.Table.HandToSaveCards(index);
-    }
-    public static void Trash(int index, List<Card> list)
-    {
-        GameEngine.bank.Add( (BankCard)list[index]);
-        list.RemoveAt(index);
-    }
-
-    public static void Attack(IPlayer Player,IPlayer Victim,int cantgem)
-    {
-        List<BankCard> result = new List<BankCard>();
-        for(int i = 0 ; i < cantgem ; i++ )
+        public static void Move ( List<Card> a , List<Card> b , int index )
         {
-            int index = Player.SelectGem();
-            result.AddRange(GameEngine.bank.GetCant(0,Player.Table.GemPile[index].Money));
+            b.Add(a[index]);
+            a.RemoveAt(index);
         }
-        Victim.Table.ToGemPile(result);
-    }
 
-    public static void GainCard ( IPlayer Player, BankCard card )
-    {
-        Player.Table.ToDiscardPile( GameEngine.bank.Get(card));
-    }
+        public static void GiveActions()
+        {
+            GameEngine.CantActionsPerTurn ++;
+        }
 
-    public static void Sacrifice(IPlayer Player, int index)
-    {
-        Move(Player.Table.HandCards,Player.Table.DiscardPile,index);
-    }
+        public static void Draw(int n,IPlayer Player)
+        {
+            Player.Table.DrawDeck(n);
+        }
 
-    public static void Revive(IPlayer Player, int index)
-    {
-        Move(Player.Table.DiscardPile,Player.Table.HandCards,index);
+        public static void SaveCards(int index,IPlayer Player)
+        {
+            Player.Table.HandToSaveCards(index);
+        }
+        public static void Trash(int index, List<Card> list)
+        {
+            GameEngine.bank.Add( (BankCard)list[index]);
+            list.RemoveAt(index);
+        }
+
+        public static void Attack(IPlayer Player,IPlayer Victim,int cantgem)
+        {
+            List<BankCard> result = new List<BankCard>();
+            List<int> indexs = new List<int>();
+            for(int i = 0 ; i < cantgem ; i++ )
+            {
+                int index = Player.SelectGem();
+                result.AddRange(GameEngine.bank.GetCant(0,Player.Table.GemPile[index].Money));
+                indexs.Add(index);
+            }
+            Victim.Table.ToGemPile(result);
+
+            indexs.Sort();
+            for(int j = indexs.Count-1; j >= 0; j--)
+            {
+                Player.Table.GemPile.RemoveAt(j);
+            }
+        }
+
+        public static void GainCard ( IPlayer Player, BankCard card )
+        {
+            Player.Table.ToDiscardPile( GameEngine.bank.Get(card));
+        }
+
+        public static void Sacrifice(IPlayer Player, int index)
+        {
+            Move(Player.Table.HandCards,Player.Table.DiscardPile,index);
+        }
+
+        public static void Revive(IPlayer Player, int index)
+        {
+            Move(Player.Table.DiscardPile,Player.Table.HandCards,index);
+        }
     }
-}
