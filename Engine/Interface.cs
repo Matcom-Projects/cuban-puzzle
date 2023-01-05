f69620d19676e226541f3184913120455f219764
Kpiro <106298160+Kpiro@users.noreply.github.com>
2023-01-05 17:09:05 -0500

    add ExecuteAction y modificacion IActionable

--- Engine/Interface.cs
+++ Engine/Interface.cs
@@ -1,30 +1,21 @@
 namespace engine_cuban_puzzle;
 
 public interface IPlayer
-{
-    public string Name { get; }
-    public TablePlayer Table { get; set; }
-    
-    public bool Exit();
-    public bool ExistIActionable();
-    public int SelectCardHand();
-    public Card SelectCardOnGoing();
-    public bool SelectField();
-    public void ChooseActionRealize(IActionable card);
-    public int SelectGem();
-    public IPlayer SelectPlayer();
-    public BankCard PlayBuyPhase();
-    public bool PlayNextBuyPhases();
-    public int SelectCardDeck();//este metodo quitarlo desp
+    {
+        public string Name { get; }
+        public TablePlayer Table { get; set; }
         
-}
-public interface IActionable
-{
-    public bool[] Actions { get; }
-    public void GiveActions();
-    public void SaveCards(int index, IPlayer a);
-    public void Draw(IPlayer a);
-    public void Attack(int index,IPlayer a);
-    public void Trash(IPlayer a);
-    public void GainCard(IPlayer a);
-}
+            public bool Exit();
+            public bool ExistIActionable();
+            public int SelectCardHand();
+            public Card SelectCardOnGoing();
+            public void ExecuteAction(IActionable card);
+            public int SelectGem();
+            public IPlayer SelectPlayer();
+            public BankCard PlayBuyPhase();
+            public bool PlayNextBuyPhases();
+    }
+    public interface IActionable
+    {
+        public void Action(IPlayer a);
+    }
