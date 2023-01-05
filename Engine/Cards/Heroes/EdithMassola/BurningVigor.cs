f469fb014218c910a54aa7ad6a99ccb56653674e
Kpiro <106298160+Kpiro@users.noreply.github.com>
2023-01-05 14:59:30 -0500

    cambios en las cartas

--- Engine/Cards/Heroes/EdithMassola/BurningVigor.cs
+++ Engine/Cards/Heroes/EdithMassola/BurningVigor.cs
@@ -1,49 +1,43 @@
 namespace engine_cuban_puzzle;
 
-public class BurningVigor : Card, IActionable
-{
-    public bool[] Actions { get; set; }
-    public BurningVigor() : base("Burning Vigor", new string[]{"red"}, 0)
+public class BurningVigor : ActionCard
     {
-        this.Actions = new bool[] {false, false, false, false, true, false};
-    }
-    public void GiveActions(){}
-    public void SaveCards(int index, IPlayer a){}
-    public void Draw(IPlayer a){}
-    public void Attack(int index,IPlayer a){}
-    public void Trash(IPlayer a)
-    {
-        if(a.Table.HandCards.Contains(new Cup()))
+        public BurningVigor() : base("Burning Vigor", new string[]{"red"}, 0)
         {
-            BankCard card = new Cup();
-            GameEngine.bank.Add(card);
-            a.Table.HandCards.Remove(card);
-            GameEngine.CantActionsPerTurn++;
-            foreach(var p in GameEngine.Turns.Players)
-            {
-                if(p==a) continue;
-                p.Table.GemPile.Add(GameEngine.bank.Get(new Gem1()));
-            }
-            return;
+            
         }
-        else if(a.Table.DiscardPile.Contains(new Cup()))
+        
+        public override void Action(IPlayer a)
         {
-            BankCard card = new Cup();
-            GameEngine.bank.Add(card);
-            a.Table.DiscardPile.Remove(card);
-            GameEngine.CantActionsPerTurn++;
-            foreach(var p in GameEngine.Turns.Players)
+            if(a.Table.HandCards.Contains(GameEngine.bank.keys[7]))
             {
-                if(p==a) continue;
-                p.Table.GemPile.Add(GameEngine.bank.Get(new Gem1()));
+                int i = a.Table.HandCards.IndexOf(GameEngine.bank.keys[7]);
+                GameActions.Trash(i,a.Table.HandCards);
+                GameActions.GiveActions();
+                foreach(var p in GameEngine.Turns.Players)
+                {
+                    if(p==a) continue;
+                    p.Table.GemPile.Add(GameEngine.bank.Get(new Gem1()));
+                }
+                return;
             }
-            return;
+            else if(a.Table.DiscardPile.Contains(GameEngine.bank.keys[7]))
+            {
+                int j = a.Table.DiscardPile.IndexOf(GameEngine.bank.keys[7]);
+                GameActions.Trash(j,a.Table.DiscardPile);
+                GameActions.GiveActions();
+                foreach(var p in GameEngine.Turns.Players)
+                {
+                    if(p==a) continue;
+                    p.Table.GemPile.Add(GameEngine.bank.Get(new Gem1()));
+                }
+                return;
+            }
         }
-    }
-    public void GainCard(IPlayer a){}
+        
 
-    /*Informacion de la carta:
-    1. Trashea un CUP de la mano o pila de descartes
-        1.1 Da una accion mas y agrega una gem1 a la gema de pila de los oponentes
-    */
-}
+        /*Informacion de la carta:
+        1. Trashea un CUP de la mano o pila de descartes
+            1.1 Da una accion mas y agrega una gem1 a la gema de pila de los oponentes
+        */
+    }
