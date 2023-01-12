71ccf6ada6d9e331e2ca896beaed5f543692268f
Ricardo Cápiro Colomar <106298160+Kpiro@users.noreply.github.com>
2023-01-12 02:33:28 -0500

    add prop Information (para cuando se implemente)

--- Engine/Cards/ClassCards.cs
+++ Engine/Cards/ClassCards.cs
@@ -7,6 +7,7 @@ public abstract class Card
     public string Name { get; private set; }
     public string Color { get; private set; }
     public int Money { get; set; }
+    public string Information {get; private set;}
     public Card ( string name,string color,int money )
     {
         this.Id = GameUtils.CreateId();
